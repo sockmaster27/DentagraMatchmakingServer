eb96397a360b84d06a9a3230e69920eef63903f4
sockmaster27 <61235930+sockmaster27@users.noreply.github.com>
2021-03-26 13:54:33 +0100

    Tilføjede TLS

--- main.py
+++ main.py
@@ -1,25 +1,43 @@
 import websockets
+import ssl
 import asyncio
 
 import secrets
 import time
 import math
+import os
 
 
 # token length does not include timestamp
 token_length = 64
 timeout = 10
 
+NO_MATCH = 4000
+
+certificate_path = os.path.join("TLS", "tls_certificate.pem")
+key_path = os.path.join("TLS", "tls_key.key")
+
 
 unpaired_socket = None
+
+
+def generate_ssl_context() -> ssl.SSLContext:
+    if not os.path.exists(key_path):
+        raise OSError(
+            f"{key_path} does not exist. Please generate new certificate, or request a copy from project owners."
+        )
+
+    else:
+        ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+        ssl_context.load_cert_chain(certificate_path, key_path)
+        return ssl_context
 
 
 async def hello(websocket, path):
     global unpaired_socket
 
     if unpaired_socket is not None:
-
-        timestamp = bytes(math.floor(time.time()))
+        timestamp = math.floor(time.time()).to_bytes(4, "big")
         token1 = secrets.token_bytes(token_length)
         token2 = secrets.token_bytes(token_length)
 
@@ -32,13 +50,16 @@ async def hello(websocket, path):
     else:
         unpaired_socket = websocket
         await asyncio.sleep(timeout)
-        if unpaired_socket == websocket:
+
+        if unpaired_socket is websocket:
             unpaired_socket = None
+            await websocket.close(code=NO_MATCH)
 
-
-start_server = websockets.serve(hello, "localhost", 2093)
+ssl = generate_ssl_context()
+start_server = websockets.serve(hello, "localhost", 2093, ssl=ssl)
 
 
 if __name__ == "__main__":
+    print("Server started")
     asyncio.get_event_loop().run_until_complete(start_server)
     asyncio.get_event_loop().run_forever()
